34f88e53a59cd435b47e235bec2b47f03a81f3e7
yakoub-75 <yakouboumarou@yahoo.com>
2020-10-16 15:22:34 +0200

    j'ai fini e travail

--- header.php
+++ header.php
@@ -4,8 +4,31 @@
 	<title></title>
 	<meta charset="utf-8"/>
 	<link rel="stylesheet" type="text/css" href="./css/style.css"/>
+	<!-- Load an icon library to show a hamburger menu (bars) on small screens -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
-	<body>
 
+	<body>
+		<div class="cadre">
+	<div class="topnav">
+		<img src="img/logo1.png" class="logo" alt="logo">
+		<input type="search" class= "search" placeholder="Recherche">
+		<img src="img/logo.png" class= "panier" alt="panier">
+ 
+  <!-- Navigation links (hidden by default) -->
+  <div id="myLinks">
+    <a href="#news">News</a>
+    <a href="#contact">Contact</a>
+    <a href="#about">About</a>
+  </div>
+  <!-- "Hamburger menu" / "Bar icon" to toggle the navigation links -->
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
+
+
+</div>
+  <script src="script.js"></script>
 	</body>
 </html>
