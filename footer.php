54b7db9ac32c89c4cab3e0d287f61263752e17c9
Migne Mathilde <mignemathilde@macbook-pro-de-migne.home>
2020-11-09 16:05:53 +0100

    fleche

--- footer.php
+++ footer.php
@@ -3,7 +3,11 @@
 
 <body>
 	<div class="footer">
-        <p>Footer</p>
+        <div id="background">
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 20 20" style= "fill: #454444">
+                <path d="M10.707 7.050l-0.707-0.707-5.657 5.657 1.414 1.414 4.243-4.242 4.243 4.242 1.414-1.414z"></path>
+            </svg>
+         </div>
 	    <a href="<?php echo $root_path ?>index.php">Index</a>
 	    <a href="<?php echo $root_path ?>php/index_marques.php">Marques</a>
 	    <a href="<?php echo $root_path ?>php/index_sellers.php">Vendeurs</a>
