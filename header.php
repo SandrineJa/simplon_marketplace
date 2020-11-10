84bef9f7acf739b9c89bd06781e3c757b0caa185
Migne Mathilde <mignemathilde@macbook-pro-de-migne.home>
2020-11-10 15:37:49 +0100

    cart

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 				<path d="M32 29c0 1.657-1.343 3-3 3s-3-1.343-3-3c0-1.657 1.343-3 3-3s3 1.343 3 3z"></path>
 				<path d="M32 16v-12h-24c0-1.105-0.895-2-2-2h-6v2h4l1.502 12.877c-0.915 0.733-1.502 1.859-1.502 3.123 0 2.209 1.791 4 4 4h24v-2h-24c-1.105 0-2-0.895-2-2 0-0.007 0-0.014 0-0.020l26-3.98z"></path>
 			</svg>
-	 
+	
 	  		<!-- Navigation links (hidden by default) -->
 	  		<div id="myLinks">
 	    		<a href="#news">Products</a>
@@ -19,8 +19,9 @@
 	    		<a href="#about">About</a>
 	  		</div>
 
-	  		<!-- "Hamburger menu" / "Bar icon" to toggle the navigation links -->
+	  		<!-- Burger menu / "Bar icon" to toggle the navigation links -->
 	  		<a href="javascript:void(0);" class="icon" onclick="myFunction()">
+	  			<!-- Icone importé du svg -->
 	    		<i class="fa fa-bars"></i>
 	  		</a>
 		</div>
