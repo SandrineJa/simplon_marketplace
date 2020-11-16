24cac89b1d1999f80f0effa73697b8c5edc71c33
Migne Mathilde <mignemathilde@macbook-pro-de-migne.home>
2020-11-16 14:16:56 +0100

    update

--- php/update_products.php
+++ php/update_products.php
@@ -1,58 +1,78 @@
 <?php 
-//connection to the database
 include('config.inc_albane.php'); 
 include('connexion.inc_albane.php');
 
 //determines the path to the root from the actual file
-$root_path = "../"; ?>
+$root_path = "../";
 
+//$_REQUEST = permet d'avoir à la fois le $_GET  et le $_POST
+if(!empty($_REQUEST['product_name'])) :
+    $brandName = $_REQUEST['product_name'];
+endif;
 
-<!DOCTYPE html>
+if(!empty($_POST['product_name'])):
+    
+    $maRequete = "UPDATE products SET
+    product_name = '".$_POST['productmodif']."'
+    WHERE product_name = '$productName' ";
+    
+    if($result = $mysqli->query($maRequete)):
+    $message = "Le produit a été modifiée avec succès";
+    else :
+    $message = "Une erreur est survenue lors de la modification ".$maRequete;
+    endif;
+endif;
+    
+
+   
+//Selection pour préremplissage du formulaire
+        if(!empty($brandName)):
+    $maRequete = "SELECT product_name FROM products WHERE product_name = '$productName'";
+            
+            if($result = $mysqli->query($maRequete)):
+        $productaModifier = $result->fetch_assoc();
+        
+            endif;
+        endif;
+    
+
+?>
 <html>
-<head>
-    <title>simplon_marketplace</title>
-    <meta charset="utf-8"/>
-    <link rel="stylesheet" type="text/css" href="<?php echo $root_path ?>css/style.css"/>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-<body>
-    <?php include('../header.php'); ?>
+    <head>
+        <title>Simplon Marketplace</title>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" type="text/css" href="<?php echo $root_path ?>css/style.css"/>
+        <!-- Load an icon library to show a hamburger menu (bars) on small screens -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <?php include('../header.php'); ?>
+    </head>
+    <body>
+        <h1>Produit</h1>
+        <?php
+        if(!empty($message)) :
+        ?>
+            <p><?php echo $message;?></p>
+        <?php
+        endif;
+        ?>
 
-<h1>Produits</h1>
-
-    <?php
-    //if there's a category in the url (when you click the update link in the index), creates a form with its actual value in a text input and a hidden input
-    if (!empty($_GET['product'])) { ?>
-
-        <form action="update_categories.php" method="POST">
-            <input type="text" name="category_update" value="<?php echo (isset($_GET['product'])) ? $_GET['product'] : ''; ?>">
-            <input type="submit" value="Valider">
-            <input type="hidden" name="product" value="<?php echo (isset($_GET['product'])) ? $_GET['product'] : ''; ?>">
+<form action="<?php $_SERVER['PHP_SELF']; ?>" method="POST">
+    
+        <label for="name"> Nom de du produit : </label>
+        <input type="text" name="productmodif" value="<?php if(!empty($productaModifier['product_name'])) echo $productaModifier['product_name']; ?>" />
+            <?php
+            
+            ?>
+            <?php
+            if(!empty($productaModifier['product_name'])):
+            ?>
+                <input type="hidden" name = "product_name" value="<?php if(!empty($productaModifier['product_name'])) echo $productaModifier['product_name']; ?>" />
+            <?php
+            endif;
+            ?>
+             <input type="submit" value="valider"/>
+            <a href="index_products.php" title="retour">retour</a>
         </form>
-    <?php  
-    }
-
-    //if the POST variable isn't empty (when you submitted a form)
-    else if (!empty($_POST['product_update'])) { 
-
-        //query that replaces the actual value by the one you put in the form
-        $update_query = "UPDATE products SET product_name = '".$_POST['product_update']."' WHERE prodeuct_name = '".$_POST['product']."'"; 
-
-        //displays a confirmation message if the query is executed
-        if ($mysqli->query($update_query)) { ?>
-            <p>Le produit "<?php echo $_POST['product_update'] ?>" a bien été modifée, cliquez sur "retour" pour voir la nouvelle liste de produits.</p>
-        <?php
-        }
-
-        //displays an error message if the query isn't executed
-        else { ?>
-            <p>Une erreur est survenue.</p>
-        <?php
-        }
-    } ?>
-
-    <a href="index_products.php" title="retour">Retour</a>
-
-    <?php include('../footer.php'); ?>
-</body>
+        <?php include('../footer.php'); ?>
+    </body>
 </html>
